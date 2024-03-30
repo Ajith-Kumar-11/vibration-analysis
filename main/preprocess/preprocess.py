61fced2a4b39f6e5871471f46e7ff32cdd687a99
Az-21 <39803478+Az-21@users.noreply.github.com>
2024-03-30 12:24:44 +0530

    Simplify sublist generator function

--- main/preprocess/preprocess.py
+++ main/preprocess/preprocess.py
@@ -1,3 +1,3 @@
 def create_sublists(data, bucket_size):
-  sublists = [data[i : i + bucket_size] for i in range(0, len(data), bucket_size)]
-  return sublists
+  _range = range(0, len(data), bucket_size)
+  return [data[i : i + bucket_size] for i in _range]
