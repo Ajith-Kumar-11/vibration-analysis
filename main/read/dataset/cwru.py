290b8705545324cf4d9adef61c4fa74d2943fdf0
Az-21 <39803478+Az-21@users.noreply.github.com>
2024-03-30 12:25:09 +0530

    Add fault code decoders

--- main/read/dataset/cwru.py
+++ main/read/dataset/cwru.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import main.read.csv
 
 
@@ -21,3 +22,58 @@ def read(path):
     faulty_dfs.append(df)
 
   return (normal_dfs, faulty_dfs)
+
+
+def decode_fault_type(fault_code):
+  fault_code = fault_code.upper()
+  main = _decode_main_fault(fault_code)
+  secondary = _decode_secondary_fault(fault_code)
+  if secondary == "outerrace":
+    outerrace_fault = _decode_outerrace_fault(fault_code)
+    secondary = f"{secondary}-{outerrace_fault}"
+  return f"{main}-{secondary}"
+
+
+def humanize_fault_diameter(value):
+  humanized = str(value)
+  humanized.replace("0", "").replace(".", "")
+  return humanized
+
+
+def _decode_main_fault(fault_code):
+  match fault_code[0]:
+    case "F":
+      return "fan"
+    case "D":
+      return "drive"
+    case _:
+      _fatal_error()
+
+
+def _decode_secondary_fault(fault_code):
+  match fault_code[2]:
+    case "I":
+      return "innerrace"
+    case "O":
+      return "outerrace"
+    case "B":
+      return "ball"
+    case _:
+      _fatal_error()
+
+
+def _decode_outerrace_fault(fault_code):
+  match fault_code[6]:
+    case "6":
+      return "c6"
+    case "R":
+      return "or3"
+    case "P":
+      return "op12"
+    case _:
+      _fatal_error()
+
+
+def _fatal_error():
+  print("Unexpected fault code in `main/read/dataset/cwru.py`. Terminating program to prevent further errors...")
+  sys.exit()
