86dad010c00234b389f467533a4f0c4eff3cff7d
Az-21 <39803478+Az-21@users.noreply.github.com>
2024-04-07 22:44:24 +0530

    Read JSON as `Config` class object

--- main/config/config.py
+++ main/config/config.py
@@ -2,13 +2,45 @@ import json
 from dataclasses import dataclass
 
 
-@dataclass
-class MyDataClass:
-  spectrogram: int
-  datasets: dict
+@dataclass(frozen=True, slots=True)
+class Spectrogram:
+  width: int
+  height: int
 
 
-def parse(json_path):
+@dataclass(frozen=True, slots=True)
+class Dataset:
+  id: int
+  name: str
+  csv_path: str
+  fft_path: str
+  frequency: int
+  bucket_size: int
+
+
+@dataclass(frozen=True, slots=True)
+class Config:
+  spectrogram: Spectrogram
+  datasets: list[Dataset]
+
+
+def parse(json_path: str = "config/config.json") -> Config:
   with open(json_path, "r") as f:
-    json_dict = json.load(f)
-  return MyDataClass(**json_dict)
+    json_data = json.load(f)
+  spectrogram_data = json_data["spectrogram"]
+  spectrogram = Spectrogram(width=spectrogram_data["width"], height=spectrogram_data["height"])
+
+  datasets = []
+  for dataset in json_data["datasets"]:
+    datasets.append(
+      Dataset(
+        id=dataset["id"],
+        name=dataset["name"],
+        csv_path=dataset["csv_path"],
+        fft_path=dataset["fft_path"],
+        frequency=dataset["frequency"],
+        bucket_size=dataset["bucket_size"],
+      )
+    )
+
+  return Config(spectrogram, datasets)
