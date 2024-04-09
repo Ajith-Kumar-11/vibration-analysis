bfe0d18be9223de790cbd7614d397166222fb0c9
Az-21 <39803478+Az-21@users.noreply.github.com>
2024-04-09 14:31:17 +0530

    Call functions necessary to generate CWRU spectrograms in main

--- main/main.py
+++ main/main.py
@@ -1,12 +1,16 @@
 import cnn.cnn as cnn
+import config.config as cfg
 import fft.fft as fft
 import gan.gan as gan
 import preprocess.preprocess as pre
-import config.config as cfg
+import read.dataset.cwru
+import workflow.cwru
 
 
 def main() -> None:
   config: cfg.Config = cfg.parse()
+  cwru_dfs = read.dataset.cwru.read_from_folder(config)
+  workflow.cwru.generate_spectrograms(cwru_dfs, config)
 
 
 if __name__ == "__main__":
