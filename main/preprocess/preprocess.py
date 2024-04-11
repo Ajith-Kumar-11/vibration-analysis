4b1da4394dfdb8e213aec0325a86e77c7b2ae457
Az-21 <39803478+Az-21@users.noreply.github.com>
2024-04-11 20:11:34 +0530

    Bugfix: drop last element of sublist if it does not have size=`bucket_size`

--- main/preprocess/preprocess.py
+++ main/preprocess/preprocess.py
@@ -1,3 +1,8 @@
 def create_sublists(data, bucket_size):
   _range = range(0, len(data), bucket_size)
-  return [data[i : i + bucket_size] for i in _range]
+  sublists = [data[i : i + bucket_size] for i in _range]
+
+  # Drop last sublist if it's not the same size as the others
+  if len(sublists[-1]) != bucket_size:
+    sublists.pop()
+  return sublists
