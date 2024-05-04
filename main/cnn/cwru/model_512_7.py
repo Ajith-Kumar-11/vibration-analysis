6ffd12d1fcfa35a7404ba779429c3649c47e7ac5
Az-21 <39803478+Az-21@users.noreply.github.com>
2024-05-04 23:21:41 +0530

    Add `config` as parameter

--- main/cnn/cwru/model_512_7.py
+++ main/cnn/cwru/model_512_7.py
@@ -7,6 +7,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torchvision
+from config.config import Config
 from loguru import logger
 from torch.autograd import Variable
 from torch.optim import Adam
@@ -14,7 +15,7 @@ from torch.utils.data import DataLoader
 from torchvision.transforms import transforms
 
 
-def run() -> None:
+def run(config: Config) -> None:
   # Prefer CUDA capable GPU if available
   device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
   logger.info(f"PyTorch device: {device}")
@@ -22,7 +23,7 @@ def run() -> None:
   # Image transformer
   transformer = transforms.Compose(
     [
-      transforms.Resize((512, 512)),  # TODO: pass this value from Config.Spectrogram
+      transforms.Resize((config.spectrogram.width, config.spectrogram.height)),
       transforms.ToTensor(),  # Convert image to tensor
       transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),  # Scale tensor values from [0, 1] to [-1, 1]
     ]
