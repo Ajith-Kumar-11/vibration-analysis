dc38d78be13f69b9a242f1b5c12b023b55e7dfae
Az-21 <39803478+Az-21@users.noreply.github.com>
2024-05-04 23:38:54 +0530

    Call CNN model in `main`

--- main/main.py
+++ main/main.py
@@ -1,6 +1,7 @@
 import config.config as cfg
 import read.dataset.cwru
 import workflow.cwru
+import cnn.cwru.model_512_7
 
 
 def main() -> None:
@@ -10,6 +11,10 @@ def main() -> None:
   if False:
     cwru_dfs = read.dataset.cwru.read_from_folder(config)
     workflow.cwru.generate_spectrograms(cwru_dfs, config)
+
+  # Train CNN model on CWRU FFT spectrograms
+  if True:
+    cnn.cwru.model_512_7.run(config)
 
 
 if __name__ == "__main__":
