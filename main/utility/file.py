0ad155d2c65b569ea786d981d4c62bb7bc6f5eae
Az-21 <39803478+Az-21@users.noreply.github.com>
2024-06-02 15:22:18 +0530

    Add utility functions' documentation

--- main/utility/file.py
+++ main/utility/file.py
@@ -1,7 +1,27 @@
+"""
+This module provides a function to count the number of files with a specific extension in a directory and its subdirectories.
+
+Functions:
+    count_files_with_extension(directory: str, extension: str) -> int: Counts the number of files with a specified extension.
+
+Dependencies:
+    os: For handling filesystem operations.
+"""
+
 import os
 
 
 def count_files_with_extension(directory: str, extension: str) -> int:
+  """
+  Counts the number of files with a specific extension in a directory and its subdirectories.
+
+  Args:
+      directory (str): The path to the directory to search for files.
+      extension (str): The extension of the files to count.
+
+  Returns:
+      int: The number of files with the specified extension.
+  """
   extension = "." + extension.lower()
   count = 0
   for _, _, files in os.walk(directory):
--- main/utility/folder.py
+++ main/utility/folder.py
@@ -1,6 +1,25 @@
+"""
+This module provides a function to ensure that a folder exists at a specified path.
+
+Functions:
+    ensure_folder_exists(path: str) -> None: Ensures that a folder exists at the specified path. If it doesn't, it creates the folder.
+
+Dependencies:
+    os: For handling filesystem operations.
+"""
+
 import os
 
 
-def ensure_folder_exists(path) -> None:
+def ensure_folder_exists(path: str) -> None:
+  """
+  Ensures that a folder exists at the specified path. If it doesn't, it creates the folder.
+
+  Args:
+      path (str): The path to the folder to be ensured.
+
+  Returns:
+      None
+  """
   if not os.path.exists(path):
     os.makedirs(path)
