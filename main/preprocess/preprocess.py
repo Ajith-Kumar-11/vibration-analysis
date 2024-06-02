1340c93726bee8dc837372d249065052d1416f07
Az-21 <39803478+Az-21@users.noreply.github.com>
2024-06-02 15:17:04 +0530

    Add documentation for preprocess.py

--- main/preprocess/preprocess.py
+++ main/preprocess/preprocess.py
@@ -1,4 +1,15 @@
 def create_sublists(data, bucket_size):
+  """
+  Splits the input data into sublists of a specified size.
+
+  Args:
+      data (list): The list of data to be split into sublists.
+      bucket_size (int): The size of each sublist.
+
+  Returns:
+      list: A list of sublists, each of size `bucket_size`. The last sublist
+            is dropped if it is not of the same size as the others.
+  """
   _range = range(0, len(data), bucket_size)
   sublists = [data[i : i + bucket_size] for i in _range]
 
